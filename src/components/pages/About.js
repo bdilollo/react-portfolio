64df87c2f5ec6fecc1ec97665014de30a2e55d00
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-05 11:35:52 -0600

    created About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                This is where I'll write a short little bio about me and my software development experience so far.
+            </p>
+        </div>
+    )
+}
+
+export default About;
