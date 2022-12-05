b321e259df39276c0344309f6b67cd0874ef4c57
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-05 11:37:05 -0600

    added proficiencies list to Resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Resume() {
+    return(
+        <div>
+            {/* need downloadable resume doc */}
+            <ul className='skills-list'>
+                <li>React</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>SQL/Sequelize</li>
+                <li>MongoDB/Mongoose</li>
+                <li>REST APIs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Resume;
