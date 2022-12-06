0b89236900d5f524ceddb2afd7a11a69b62f7a55
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-05 23:31:33 -0600

    added submit button + cleaned up error alert logic

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -49,7 +49,7 @@ function Contact() {
 
     return(
         <div>
-            <h1>Contact</h1>
+            <h2>Contact</h2>
             <form className="contact-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
