329e9b60a9181b7afdc42f9e3f29cfdf47cc8f49
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-05 23:30:51 -0600

    added pic + short bio to About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,14 @@ import React from 'react';
 function About() {
     return (
         <div>
-            <h1>About Me</h1>
+            <h2>About Me</h2>
+            <img 
+                className='img-thumbnail'
+                style={{ width: "10rem" }}
+                src="images/portfolio-photo.jpg" 
+                alt="recent"/>
             <p>
-                This is where I'll write a short little bio about me and my software development experience so far.
+                Hi! My name is Bradley, and I'm just beginning my journey into the world of software development. I'm currently a student in the Full-Stack bootcamp through UC Berkeley Extension. I've still got a ton to learn, but I'm really excited to continue expanding my skills and becoming a software developer!
             </p>
         </div>
     )
